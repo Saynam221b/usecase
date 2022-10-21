d20e038190cb8783d8199d9d13d275dc2f6dfdf5
Saynam221b <saynam1101@gmail.com>
2022-10-21 17:52:17 +0530

    commit for 21 oct task

--- client/src/main/java/com/insurance/client/ClientController.java
+++ client/src/main/java/com/insurance/client/ClientController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class ClientController {
 
@@ -16,16 +14,41 @@ public class ClientController {
 	private ClientRepo cr;
 	
 
-	
 	@GetMapping("/details/id={id}") 
 	
 	public List<ClientEntity> getSample(@PathVariable("id") Integer id){
 		
     	
-		 return cr.getjoin(id);
+		 return cr.getJoin(id);
 		 
 		 //http://localhost:8080/details/id=3 to get value of id 3
 		
+	}
+	
+	private  int discount=0;
+	@GetMapping("/client_discount/{tenture}/{premium}")
+    public int discount(@PathVariable String tenture, @PathVariable int premium) {
+        final String more = "morethanfive";
+        final String less = "lessthanfive";
+        final String equal = "equalfive";
+       
+        if(tenture.equals(less)) {
+            return premium;
+        }
+        
+        else if(tenture.equals(equal)) {
+            discount = 10*premium/100;
+            return premium-discount;
+        }
+        
+        else if(tenture == more) {
+
+            discount =12*premium/100;
+            return premium-discount;
+        
+    }
+        return premium;
+        
 	}
 
 	@GetMapping("/details") 
@@ -33,7 +56,7 @@ public class ClientController {
 	public List<ClientEntity> getSample2(){
 	
 	
-		return cr.getjoin2();
+		return cr.getJoin2();
 		//http://localhost:8080/details for all the details
 	
 }
--- client/src/main/java/com/insurance/client/ClientRepo.java
+++ client/src/main/java/com/insurance/client/ClientRepo.java
@@ -11,11 +11,11 @@ import org.springframework.data.jpa.repository.Query;
 public interface ClientRepo extends JpaRepository<ClientEntity, Integer>{
 	 
 	@Query(value = "select * from client_details c where c.client_id = ?1",nativeQuery = true)
-	List<ClientEntity> getjoin(Integer client_id);
+	List<ClientEntity> getJoin(Integer client_id);
 	
 	
 	@Query(value = "select * from client_details",nativeQuery = true)
-	List<ClientEntity> getjoin2();
+	List<ClientEntity> getJoin2();
 
 	
    
